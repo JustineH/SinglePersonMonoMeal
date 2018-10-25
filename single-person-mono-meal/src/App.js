9f4af4ec05b9710cebaafb143583987702859cf7
Justine <justine@later.com>
2018-10-25 11:23:28 -0700

    Add stepOne and docScroller div classes

--- single-person-mono-meal/src/App.js
+++ single-person-mono-meal/src/App.js
@@ -6,13 +6,18 @@ import Button from '@material-ui/core/Button';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          // <img src={logo} className="App-logo" alt="logo" />
-          <h2>
-            Single Person Mono Meal 
-          </h2>
-        </header>
+      <div className="docScroller">
+        <div className="App">
+          <header className="App-header">
+            <h2>
+              Single Person Mono Meal 
+            </h2>
+          </header>
+        </div>
+        <div className="stepOne">
+          <div className="inside">
+          </div>
+        </div>
       </div>
     );
   }
