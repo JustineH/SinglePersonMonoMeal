1c5acd1522634490454645d1691457c7a65ebce6
Justine <justine@later.com>
2018-10-25 16:14:06 -0700

    Add grid and sections

--- single-person-mono-meal/src/App.js
+++ single-person-mono-meal/src/App.js
@@ -2,36 +2,64 @@ import React, { Component } from 'react';
 import * as Scroll from 'react-scroll';
 import logo from './logo.svg';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
 import imageCerealAndMilk from './cereal-and-milk.svg'; 
+import imageBowlOfCereal from './nyana-stoica-601258-unsplash.jpg';
+import imageHungry from './noun_hungry_1926312.svg';
+import imageBowl from './noun_Bowl_1460102.svg';
+import imageMilk from './milk.svg';
+import imageSpoon from './noun_Spoon_1825509.svg';
+import imageCerealAndBowl from './cereal-and-bowl.svg';
+import imageCerealWhite from './cereal-white.svg';
 import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
-      <div className="docScroller">
-        <div className="App">
-          <header className="App-header">
-          <img src={imageCerealAndMilk} className="App-logo" alt="logo" />
-            <h2>
-              Single Person Mono Meal 
-            </h2>
-          </header>
-        </div>
-        <div className="stepOne">
-          <div className="inside">
-            <h2>
-              Step One: Grab a bowl
-            </h2>
+      <Grid container spacing={24}>
+        <div className="docScroller">
+          <Grid item xs={12}>
+            <div className="App">
+              <header className="App-header">
+              <img src={imageCerealAndMilk} className="App-logo" alt="logo" />
+                <h2>
+                  Single Person Mono Meal 
+                </h2>
+              </header>
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <div className="hungry">
+              <div className="inside">
+              <img src={imageHungry} className="iconImage" />
+                <h1>
+                  Hunger
+                </h1>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+          <div className="stepOne">
+            <div className="inside">
+              <h1>
+                Step One: Grab a bowl
+              </h1>
+              <img src={imageBowl} className="iconImage" />
+            </div>
           </div>
-        </div>
-        <div className="stepTwo">
-          <div className="inside">
-            <h2>
-              Step Two: Grab a spoon
-            </h2>
+          </Grid>
+          <Grid item xs={12}>
+          <div className="stepTwo">
+            <div className="inside">
+            <img src={imageSpoon} className="iconImage" />
+              <h1>
+                Step Two: Grab a spoon
+              </h1>
+            </div>
           </div>
+          </Grid>
         </div>
-      </div>
+      </Grid>
     );
   }
 }
