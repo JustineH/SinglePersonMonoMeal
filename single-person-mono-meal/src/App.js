ee7c12e6f8b1dbdfbb007ce9ddabc0458fe95935
Justine <justine@later.com>
2018-10-26 11:33:06 -0700

    Add remaining steps

--- single-person-mono-meal/src/App.js
+++ single-person-mono-meal/src/App.js
@@ -4,7 +4,6 @@ import logo from './logo.svg';
 import './App.css';
 import Grid from '@material-ui/core/Grid';
 import imageCerealAndMilk from './cereal-and-milk.svg'; 
-import imageBowlOfCereal from './nyana-stoica-601258-unsplash.jpg';
 import imageHungry from './noun_hungry_1926312.svg';
 import imageBowl from './noun_Bowl_1460102.svg';
 import imageCerealBox from './noun_Cereal Box Open_434709.svg';
@@ -12,47 +11,45 @@ import imageMilk from './milk-bottle.svg';
 import imageSpoon from './noun_Spoon_1825509.svg';
 import imageCerealAndBowl from './cereal-and-bowl.svg';
 import imageEat from './noun_porridge_1997682.svg';
+import imageHappy from './noun_jump_1682137.svg';
 import Button from '@material-ui/core/Button';
 
 class App extends Component {
 
   componentDidMount(args) {
-    // super(args)
     document.querySelector(".docScroller")
     .addEventListener("scroll", (event) => {
       let scrollTop = event.target.scrollTop
       let images = document.querySelectorAll("img")
       images.forEach((image) => {
-        // image.style.transform = "rotate(" + scrollTop + "deg)"
-        image.style.transform = `rotate(${scrollTop*2}deg)`
+        image.style.transform = `rotateZ(${scrollTop/10 % 3}deg)`
       })
       console.log(scrollTop)
       console.log(images)
-
     })
   }
 
   render() {
     return (
-      <Grid container spacing={24}>
+      <Grid container>
         <div className="docScroller">
           <Grid item xs={12}>
             <div className="App">
               <header className="App-header">
               <img src={imageCerealAndMilk} className="App-logo" alt="logo" />
-                <h2>
+                <h1>
                   Single Person Mono Meal 
-                </h2>
+                </h1>
               </header>
             </div>
           </Grid>
           <Grid item xs={12}>
             <div className="hungry">
               <div className="inside">
-              <img src={imageHungry} className="iconImage" />
                 <h1>
                   Hunger
                 </h1>
+                <img src={imageHungry} className="iconImage" />
               </div>
             </div>
           </Grid>
@@ -60,8 +57,11 @@ class App extends Component {
           <div className="stepOne">
             <div className="inside">
               <h1>
-                Step One: Grab a bowl
+                Step One: 
               </h1>
+              <h2>
+                Grab a bowl
+              </h2>
               <img src={imageBowl} className="iconImage" />
             </div>
           </div>
@@ -69,40 +69,65 @@ class App extends Component {
           <Grid item xs={12}>
           <div className="stepTwo">
             <div className="inside">
-            <img src={imageSpoon} className="iconImage" />
               <h1>
-                Step Two: Grab a spoon
+                Step Two: 
               </h1>
+              <h2>
+                Grab a spoon
+              </h2>
+              <img src={imageSpoon} className="iconImage" />
             </div>
           </div>
           </Grid>
           <Grid item xs={12}>
           <div className="stepThree">
             <div className="inside">
-            <img src={imageCerealBox} className="iconImage" />
               <h1>
-                Step Three: Grab a box of your favourite cereal (or whatever you have in the pantry)
+                Step Three: 
               </h1>
+              <h2>
+                Grab a box of your favourite cereal
+              </h2>
+              <img src={imageCerealBox} className="iconImage" />
             </div>
           </div>
           </Grid>
           <Grid item xs={12}>
           <div className="stepFour">
             <div className="inside">
-            <img src={imageMilk} className="iconImage" />
               <h1>
-                Step Four: Pour milk into your bowl 
+                Step Four: 
               </h1>
+              <h2>
+                Pour milk onto cereal
+              </h2>
+              <img src={imageMilk} className="iconImage" />
             </div>
           </div>
           </Grid>
           <Grid item xs={12}>
           <div className="stepFive">
             <div className="inside">
-            <img src={imageEat} className="iconImage" />
               <h1>
-                Step Five: Dig in!
+                Step Five: 
               </h1>
+              <h2>
+                Dig in!
+              </h2>
+              <img src={imageEat} className="iconImage" />
+            </div>
+          </div>
+          </Grid>
+          <Grid item xs={12}>
+          <div className="stepSix">
+            <div className="inside">
+              <h1>
+                Step Six:
+              </h1>
+              <h2>
+                See Step One
+              </h2>
+              <img src={imageHappy} className="iconImage" />
             </div>
           </div>
           </Grid>
